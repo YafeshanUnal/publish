3efd108a9cf3e450a3603d58a76de325ed642911
YafeshanUnal <yafeshan01@gmail.com>
2022-10-24 10:52:59 +0300

    a

---
+++ ClientApp/build/precache-manifest.be3329983af9fde14b65f81516e15c67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f343187e00899b48685873cdf957e49a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d61252ed2d460f3f003",
+    "url": "/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "d260fa980d71c6742bbb",
+    "url": "/static/css/main.ccfd5d35.chunk.css"
+  },
+  {
+    "revision": "0d61252ed2d460f3f003",
+    "url": "/static/js/2.fdb5a310.chunk.js"
+  },
+  {
+    "revision": "d0e457ec4a0285a7f6495217d65e7434",
+    "url": "/static/js/2.fdb5a310.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d260fa980d71c6742bbb",
+    "url": "/static/js/main.cc4436eb.chunk.js"
+  },
+  {
+    "revision": "4dfe17903240805a1bc6",
+    "url": "/static/js/runtime-main.968bbd1f.js"
+  }
+]);
